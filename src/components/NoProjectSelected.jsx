df0ee616e833c2d6137474e1d199e22de8f1a579
Ana Saric <saric.ana456@gmail.com>
2023-11-21 16:03:59 +0100

    Added components for creating a new project

---
+++ src/components/NoProjectSelected.jsx
@@ -0,0 +1,14 @@
+function NoProjectSelected() {
+  return (
+    <div>
+      <img />
+      <h2>No Project Selected</h2>
+      <p>Select a project or get started with a new one</p>
+      <p>
+        <button>Create new project</button>
+      </p>
+    </div>
+  );
+}
+
+export default NoProjectSelected;
